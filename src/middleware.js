cb9c9e1d988e0a15bd69921bbee5eebe85b9ac2c
y1aszz <yasmim.sordi@estudante.ifms.edu.br>
2023-09-12 17:40:37 -0300

    comentando o middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,21 +1,21 @@
 'use server'
-import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";//importação
 import { validateToken } from "./app/functions/validateToken";
 
-export const middleware = (request) => {
+export const middleware = (request) => {//obtem o token do cookie, se existir
 
-    const token = request.cookies.get('token')?.value;
-    const urlLogin = new URL('/', request.url);
-    const isTokenValidated = validateToken(token);
+    const token = request.cookies.get('token')?.value;//cria uma instancia da URL da requisição
+    const urlLogin = new URL('/', request.url);//valida token
+    const isTokenValidated = validateToken(token);//verifica se o token n é valido ou se esta ausente
 
-    if (!isTokenValidated || !token) {
-        if (request.nextUrl.pathname === '/pages/dashboard') {
+    if (!isTokenValidated || !token) {//verifica se a proxima URL (request.nextUrl) é /pages/dashboard'
+        if (request.nextUrl.pathname === '/pages/dashboard') {//redireciona para a URL de login
             return NextResponse.redirect(urlLogin);
         }
     }
-    NextResponse.next();
+    NextResponse.next();//é chamada a função NextResponse.next() para continuar o fluxo normal de execução, permitindo que a aplicação siga para o próximo middleware ou rota.
 };
-export const config = {
-    matcher: ['/', '/pages/dashboard']
+export const config = {//objeto que esta sendo exportado
+    matcher: ['/', '/pages/dashboard']//configura o matcher para corresponder às rotas '/' (raiz) e '/pages/dashboard'.
 };
 
