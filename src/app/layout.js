91d4668539671b5919edcb6225af547141c0cb81
y1aszz <yasmim.sordi@estudante.ifms.edu.br>
2023-09-12 17:33:07 -0300

    comentando layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,19 @@
 
 
-export const metadata = {
-  title: 'Register users',
-  description: 'Project for class PTAC',
+export const metadata = {//define um objeto chamado metadata
+  title: 'Register users',//define o título de uma página ou de algum componente específico.
+  description: 'Project for class PTAC',//fornece uma descrição ou contexto adicional sobre o objeto ou componente ao qual esses metadados se aplicam.
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }) {//
   return (
-    <html lang="pt-br">
+    <html lang="pt-br">{/*define a lingua*/}
       <body>{children}</body>
+      {/* 
+      Qualquer conteúdo que você deseje renderizar nas páginas do seu aplicativo
+      (como componentes, texto, imagens, etc.) será inserido dentro deste elemento
+      <body> quando você usar o componente RootLayout.
+      */}
     </html>
   )
 }
