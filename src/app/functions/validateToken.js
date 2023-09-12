0334313bd9a79fce7f8b5a279dc2f1fbdfb637e3
y1aszz <yasmim.sordi@estudante.ifms.edu.br>
2023-09-12 17:57:32 -0300

    comentando validateToken

--- src/app/functions/validateToken.js
+++ src/app/functions/validateToken.js
@@ -1,9 +1,10 @@
-import { decode } from "jsonwebtoken";
-const validateToken = (token)=>{
-    const isTokenValidate = decode(token);
+import { decode } from "jsonwebtoken";//importação
+
+const validateToken = (token)=>{//usada para validar o token JWT fornecido.
+    const isTokenValidate = decode(token);//Dentro da função, a linha acima chama a função decode da biblioteca jsonwebtoken para tentar decodificar o token JWT. O resultado da decodificação é armazenado na variável 
     if(isTokenValidate){
-        return token= true
+        return token= true//se for true (verdadeira) indica que o token passou na validação
     }
-    return token=false;
+    return token=false;//se for false (falsa) indica que o token falhou
 }   
 export {validateToken};
