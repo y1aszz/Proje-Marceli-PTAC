07ea467c10e27a63dd80f8e8edb8ab8e36983219
y1aszz <yasmim.sordi@estudante.ifms.edu.br>
2023-09-13 01:38:53 -0300

    criação do register e comentando

---
+++ src/app/pages/dashboard/registra/registra.js
@@ -0,0 +1,45 @@
+'use client'
+import { useState } from "react";//importações
+import { useRouter } from 'next/navigation';
+
+export default function RegistrarUsers(){
+    const router = useRouter();//cria uma variavel chamada router que armazena uma instancia do roteador Next.js
+    const [name, setName] = useState ("");// Isso representa o estado do campo de nome no formulário de registro. O valor inicial é definido como uma string vazia, "".
+    const [password, setPassword] = useState ("");//o mesmo serve para o password e email 
+    const [email, setEmail] = useState("");
+
+    const register = (e) => {// (e) é um parametro da função que representa um evento
+        e.preventDefault()//método que impede o comportamento padrão do evento.
+        const user = {//cria um objeto chamado user
+            name: name,
+            password: password,
+            email: email
+        }
+    }
+
+    return(//retorna um HTML
+        <div className="container">
+            <h1 className="regis">Registrar Usuarios</h1>
+            
+            <form className="formRegis" onSubmit={register}>
+                
+                <div className="name">
+                    <input id="nomeRegis" placeholder="Digite seu nome" type="text" name="name" value={name}//nome que foi colocado no user
+                    onChange={e => setName(e.target.value)}/>
+                </div>
+
+                <div className="email">
+                <input id="emailRegis" placeholder="Digite seu email" type="email" name="name" value={email}//email que foi colocado no user
+                    onChange={e => setEmail(e.target.value)}/>
+                </div>
+
+                <div className="password">
+                <input id="passwordRegis" placeholder="Digite sua senha" type="password" name="password" value={password}//password que foi colocado no user
+                    onChange={e => setPassword(e.target.value)}/>
+                </div>
+
+                <button className="buttonRegis" type="submit">Registrar</button>
+            </form>
+        </div>
+    )
+}
