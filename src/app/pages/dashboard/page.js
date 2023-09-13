311b56d252afd504e0d1c48e263c58c2c3b2ef43
y1aszz <yasmim.sordi@estudante.ifms.edu.br>
2023-09-13 02:11:14 -0300

    dasboard e comentario

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,11 +1,16 @@
-import { getUsers } from "@/app/functions/handlerAcessAPI";
+import { getUsers } from "@/app/functions/handlerAcessAPI";//importação
 
 export default async function Dashboard() {
-   
+   const users = getUsers();//retorna a lista de usuarios estabelecida no handlerAcessAPI
     return (
         <div>
             <h1>Dashboard</h1>
-            
+            <h3 className="h3Lista">Lista de Usuarios:</h3>
+            <ul>
+                {users?.map((user, index) => (
+                    <li key={index}>{user.name}, {user.email}</li>//vai exibir as informações nome e email que foram pegas do formulario que criamos no js
+                ))}
+            </ul>
         </div>
     );
 };
