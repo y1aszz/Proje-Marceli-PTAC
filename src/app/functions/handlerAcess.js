1803d6fca1d6421acd733f341194b7b1b3e346f0
y1aszz <yasmim.sordi@estudante.ifms.edu.br>
2023-09-13 02:07:33 -0300

    handlerAcess comentada e handlerAcessAPI

--- src/app/functions/handlerAcess.js
+++ src/app/functions/handlerAcess.js
@@ -1,17 +1,17 @@
-import Cookies from "js-cookie";
+import Cookies from "js-cookie";//importações
 import { validateToken } from "./validateToken";
 import { getUserAuthenticated } from "./handlerAcessAPI";
 
-const handlerAcessUser = async (user) => {
+const handlerAcessUser = async (user) => {//async função assincrona, o user é um parametro da função, a função espera para receber o objeto 'user'
 
-    const userAuth = await getUserAuthenticated(user);
+    const userAuth = await getUserAuthenticated(user);//await é para esperar a conclusão de uma operação. Chama a função getUser e passa o objeto user
     
-    const isTokenValidate = validateToken(userAuth.token);
+    const isTokenValidate = validateToken(userAuth.token);//valideToken passa o token obtido da autenticação
 
-    if (isTokenValidate) {
-        Cookies.set('token', userAuth.token, { expires: 1 });
-        localStorage.setItem('name', userAuth.name);
-        return getUserAuthenticated
+    if (isTokenValidate) {//inicia uma verificação se a variável isTokenVali é verdadeira. Se for true, o código dentro será executado; caso contrário, será ignorado.
+        Cookies.set('token', userAuth.token, { expires: 1 });//esta configurando um cookie chamado token. O expires: 1 configura o cookie para expirar após 1 dia
+        localStorage.setItem('name', userAuth.name);//userAuth armazena o nome do usuário. localStorange permite que acesse o nome do usuario sem solicitação de rede   
+        return getUserAuthenticated//retorna a função getUser
     }
 }
 export default handlerAcessUser;
--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,10 +1,22 @@
 'use server'
 
-const getUserAuthenticated = (user) => {
+const users = [//criando um afunção chamada users
+    {
+        name: "yas",
+        email: "yasLinda@gmail.com",
+        password: "1234",
+        token: ""
+    }
+];
 
+const getUserAuthenticated = (user) => {
+    return users.find(u => u.email === user.password && u.password ===user.password);
+    //users.find tenta encontrar um usuario na lista
+    //compara o email fornecido pelo usuario com o campo 'password' na lista de usuarios
+    //compara a senha fornecida pelo usuario com o campo 'password' na lista de usuarios
 }
 
-const getUsers = () =>{
-        
+const getUsers = () =>{//cria a função getUsers
+        return users//retorna as informações que estao dentro da função 'users'
 }
 export { getUsers, getUserAuthenticated };
