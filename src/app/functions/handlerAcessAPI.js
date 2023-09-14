86c3f07a933116a15204ecb72e6e66b8b9903518
y1aszz <yasmim.sordi@estudante.ifms.edu.br>
2023-09-14 00:10:10 -0300

    token

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -5,12 +5,12 @@ const users = [//criando um afunção chamada users
         name: "yas",
         email: "yasLinda@gmail.com",
         password: "1234",
-        token: ""
+        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
     },
     {
         name: "jen",
         email: "jenzinha@gmail.com",
-        token: ""
+        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
     }
 ];
 
