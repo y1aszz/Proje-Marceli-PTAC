8b5edceaf9b0fd3570815f29ef3b5d7d26b40570
y1aszz <yasmim.sordi@estudante.ifms.edu.br>
2023-10-17 17:06:22 -0300

    update API

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,39 +1,18 @@
 'use server'
+const url="https://aula-17-10-m2vq19h7a-y1aszz.vercel.app";
 
-const users = [//criando um afunção chamada users
+const getUserAuthenticated = async (user) => {
+   const responseOfApi = await fetch(url + "/user/authenticated",
     {
-        name: "yas",
-        email: "yasLinda@gmail.com",
-        password: "1234",
-        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    },
-    {
-        name: "jen",
-        email: "jenzinha@gmail.com",
-        password: "321",
-        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    },
-    {
-        name: "marcelino",
-        email: "marcelina@gmail.com",
-        password: "123",
-        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    }
-];
-
-const getUserAuthenticated = (user) => {
-    let userAuth = [];
-    users.map(u => {  
-        if( u.email === user.email && u.password === user.password){
-          userAuth = u
-        }});
-      return userAuth//retorna os usuarios da lista
-    //users.find tenta encontrar um usuario na lista
-    //compara o email fornecido pelo usuario com o campo 'password' na lista de usuarios
-    //compara a senha fornecida pelo usuario com o campo 'password' na lista de usuarios
+        method: "POST",
+        header: {"Content-Type": "application/json"},
+        body: JSON.stringify(user)
+    });
+    const useAuth = await responseOfApi.json();
+    return useAuth;
 }
 
-const getUsers = () =>{//cria a função getUsers
-        return users//retorna as informações que estao dentro da função 'users'
+const getUsers = async () =>{
+      
 }
 export { getUsers, getUserAuthenticated };
