b0fef3e4513a3b11b6c08e7a75dc7b62068021f2
y1aszz <yasmim.sordi@estudante.ifms.edu.br>
2023-11-01 00:23:40 -0300

    arrumei

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,12 +1,12 @@
 'use server'
-const url="https://aula-17-10-2fa7.vercel.app/";
+const url="https://aula-17-10-2fa7.vercel.app";
 
 const getUserAuthenticated = async (user) => {
     console.log(user)
    const responseOfApi = await fetch(url + "/user/authenticated",
     {
         method: "POST",
-        headers: {"Content-Type": "application/json"},
+        headers: {"Content-Type": "Application/json"},
         body: JSON.stringify(user)
     });
     const useAuth = await responseOfApi.json();
